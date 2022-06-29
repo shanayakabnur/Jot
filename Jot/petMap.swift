665961c2aa9c02e5e44ce4471f754ebfb69cdd95
Shanaya Kabnur <skabnur66@gmail.com>
2022-06-29 13:06:26 -0500

    -added additional map pins

--- Jot/petMap.swift
+++ Jot/petMap.swift
@@ -14,27 +14,47 @@ class petMap: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.mapView.delegate = self
-
         // Do any additional setup after loading the view.
-        
-        // set an initial location
-        let sourceLocation = CLLocationCoordinate2D(latitude: 29.737600, longitude: -95.588080)
-        //add map pin
-        let sourcePlacemark = MKPlacemark(coordinate: sourceLocation, addressDictionary: nil )
-        //label the map pin
-        let sourceAnnotation = MKPointAnnotation()
-                sourceAnnotation.title = "Lakeside Animal Clinic"
-        sourceAnnotation.subtitle = "Emergency and non-emergency animal clinic. Open 7am - 6pm M-F; 8am - 12pm Sat; closed Sundays."
-        
-        if let location = sourcePlacemark.location{
-                    sourceAnnotation.coordinate = location.coordinate
+               createAnnotations(locations: annotationLocations)
+           }
+           // annotation locations
+           let annotationLocations = [
+            ["title": "Lakeside Animal Clinic", "subtitle": "Non-emergency and emergency vet. Open 7am - 6pm M-F; 8am - 12pm Sat.", "latitude": 29.737600, "longitude": -95.588080],
+            
+            ["title": "Rice Village Animal Hospital", "subtitle": "Non-emergency vet. Open 8am - 6pm M-F", "latitude":  29.717540, "longitude":  -95.413120],
+            
+            ["title": "Washington Heights Veterinary Clinic", "subtitle": "Non-emergency vet. Open 7:30am - 6pm M-F; 8am - 12pm Sat.", "latitude":  29.774750, "longitude":  -95.382240],
+            
+            ["title": "Midtown Veterinary Hospital", "subtitle": "Non-emergency vet. Open 7:30am - 6pm M-F; 8am - 12pm Sat.", "latitude":  29.738950, "longitude":  -95.397170],
+            
+            ["title": "Mission Bend Animal Clinic", "subtitle": "Non-emergency vet. Open 7:30am-6pm M-F; 8am-5pm Sat.", "latitude":  29.700640, "longitude":  -95.652687],
+            
+            ["title": "Briarcrest Veterinary Clinic", "subtitle": "Non-emergency vet. Open 7am-6pm M-F; 8:30am-12pm Sat.", "latitude":  29.7499695658148, "longitude":  -95.5721949055742],
+            
+            ["title": "Beechnut Animal Hospital", "subtitle": "Emergency vet. Open 7am-6pm M-F; 8am - 12pm Sat.", "latitude":  29.690945, "longitude": -95.535613],
+            
+            ["title": "VCA Ashford Animal Clinic", "subtitle": "Emergency vet. Open 7am-6pm M-F; 8am - 12pm Sat.", "latitude":  29.746214, "longitude":  -95.604795],
+            
+            ["title": "Oak Forest Veterinary Hospital", "subtitle": "Emergency vet. Open 7am-6pm M-F; 7:30am - 11:30pm Sat.", "latitude":  29.8173221134128, "longitude":  -95.4372826452034]
             
             
-                }
+            
+               
+               
+           ]
+
+           //create annotation
+           //[Dictionary<String, Any>] or [[String: Any]] works for argument type
+           func createAnnotations(locations: [[String : Any]]) {
+               for location in locations {
+                   let annotations = MKPointAnnotation()
+                   annotations.title = location["title"] as? String
+                   annotations.subtitle = location ["subtitle"] as? String
+                   annotations.coordinate = CLLocationCoordinate2D(latitude: location["latitude"] as! CLLocationDegrees, longitude: location["longitude"] as! CLLocationDegrees)
+                   mapView.addAnnotation(annotations)
+               }
         
-        // show region of choice
-                self.mapView.showAnnotations([sourceAnnotation] , animated: true)
+       
         
     }
     
