9e61fc11bbc30522bdee2a0b79362ea5cdbba0fe
Shanaya Kabnur <skabnur66@gmail.com>
2022-06-30 15:47:42 -0500

    a lot of stuff

--- Jot/HomePagePetViewController.swift
+++ Jot/HomePagePetViewController.swift
@@ -9,14 +9,29 @@ import UIKit
 
 class HomePagePetViewController: UIViewController {
 
+    @IBOutlet weak var petFindButton: UIButton!
+    @IBOutlet weak var fidoButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        fidoButton.addTarget(self, action: #selector(openLink), for: .touchUpInside)
+        petFindButton.addTarget(self, action: #selector(openLinkk), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
     
-    
-
+    @objc func openLink() {
+        if let urlToOpen = URL(string: "https://bringfido.com"){
+            UIApplication.shared.open(urlToOpen, options: [:]){ (done) in
+                
+            }
+        }
+    }
+    @objc func openLinkk() {
+        if let urlToOpen = URL(string: "https://petfinder.com"){
+            UIApplication.shared.open(urlToOpen, options: [:]){ (done) in
+                
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
---
+++ Jot/julyViewController.swift
@@ -0,0 +1,46 @@
+//
+//  julyViewController.swift
+//  Jot
+//
+//  Created by Shanaya Kabnur on 6/30/22.
+//
+
+import UIKit
+
+class julyViewController: UIViewController{
+    
+    
+    @IBOutlet weak var waterTextField: UITextField!
+    
+    let waters = ["1 cup", "2 cups", "3 cups", "4 cups", "5 cups", "6 cups", "7 cups", "8 cups", "8+ cups"]
+    
+    var pickerView = UIPickerView()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        waterTextField.inputView = pickerView
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension julyViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return waters.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return waters[row]
+        
+    }
+    
+    func pickerView (_ pickerView : UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        waterTextField.text = waters[row]
+        waterTextField.resignFirstResponder()
+    }
+    
+}
